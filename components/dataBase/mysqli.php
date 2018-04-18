4a23ff63dce1f6d9e2d37217ecc09e3edb163e93
sketch47 <hamhb777@gmail.com>
2018-04-18 18:33:17 +0300

    Объединены вывод страниц(теперь используеться один шаблон для вывода news и stati) можно добавлять свои разделы и выводить их с помощью функции, добавленна функция добавляющая css файл в <head>, мелкие правки, в админке можно создовать статьи, редактировать и удалять их

--- components/dataBase/mysqli.php
+++
@@ -1,9 +0,0 @@
-<?php
-function mysqlli(){
-    $mysqli = new mysqli('127.0.0.1','blant','1234','math');
-
-    if ($mysqli->connect_error) {
-        die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-    }
-    return $mysqli;
-}
